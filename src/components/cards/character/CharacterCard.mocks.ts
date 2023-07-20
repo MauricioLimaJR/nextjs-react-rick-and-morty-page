39e19063e195dcf550c00a9258aed0a9daf7ddf1
Mauricio Lima <jmauriciolm38@gmail.com>
2023-07-20 01:34:20 -0300

    feature: Character Card props update

--- src/components/cards/character/CharacterCard.mocks.ts
+++ src/components/cards/character/CharacterCard.mocks.ts
@@ -1,8 +1,10 @@
 import { ICharacterCard } from './CharacterCard';
 
 const base: ICharacterCard = {
+  id: 1,
   name: 'Rick Sanchez',
   specie: 'Human',
+  onCardClick: () => {},
 };
 
 export const mockCharacterCardProps = {
--- src/components/cards/character/CharacterCard.tsx
+++ src/components/cards/character/CharacterCard.tsx
@@ -5,29 +5,31 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
 export interface ICharacterCard {
+  id: number;
   name: string;
   specie: string;
+  onCardClick: () => void;
 }
 
-const CharacterCard: React.FC<ICharacterCard> = ({ name, specie }) => {
+const CharacterCard: React.FC<ICharacterCard> = (props) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
+      <CardActionArea onClick={props.onCardClick}>
         <CardMedia
           component="img"
           height="200"
           image="https://rickandmortyapi.com/api/character/avatar/69.jpeg"
-          alt={`${name} picture`}
+          alt={`${props.name} picture`}
         />
         <CardContent>
           <Chip
-            label={specie}
+            label={props.specie}
             color="secondary"
             size="small"
             variant="outlined"
           />
           <Typography gutterBottom variant="h5" component="div">
-            {name}
+            {props.name}
           </Typography>
         </CardContent>
       </CardActionArea>
